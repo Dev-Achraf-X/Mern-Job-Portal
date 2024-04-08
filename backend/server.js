828e6878e81846cc64003a899b30c4a52be44fd9
Achrafdiv <elkahanachraf57@gmail.com>
2024-04-08 18:40:47 +0000

    setup for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 
 // import routes
@@ -9,16 +10,23 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // database connection
 connectToMongoDB();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors(process.env.CLIENT_BASE_URL, { credentials: true }));
 
 // Routes
 app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // Global Error handeling
 app.use((err, req, res, next) => {
